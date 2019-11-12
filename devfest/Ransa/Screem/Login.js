504f4c98a1f9651a356e5aeeb53d0ec23157af8d
jorgelisari1 <jorgelis_ari@hotmail.com>
2019-11-12 18:29:33 -0500

    add login review

--- devfest/Ransa/Screem/Login.js
+++ devfest/Ransa/Screem/Login.js
@@ -1,28 +1,36 @@
-import React, { Component } from "react";
-import { Image } from "react-native";
-import { Container, Header, Content, Form, Item, Input, Label, Button, Text } from "native-base";
-
-export default class FixedLabelExample extends Component {
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import { Container, Content, Item, Input,Button, Text, CardItem, Card, Body} from 'native-base';
+export default class login extends Component {
   render() {
     return (
       <Container>
-        <Header />
-        <Content>
-          <Image source={require("../img/logo-1.png")} style={{ width: 160, height: 30 }} />
-          <Form>
-            <Item fixedLabel>
-              <Label>Username</Label>
-              <Input />
-            </Item>
-            <Item fixedLabel last>
-              <Label>Password</Label>
-              <Input />
-            </Item>
-          </Form>
-          <Button success onPress={() => navigate()}>
-            <Text> Success </Text>
-          </Button>
+       
+        <Content padder style= {{ justifyContent:"center", alignItems:"stretch", flex:1}}>
+         <Card>
+           <CardItem  bordered> 
+           <Image source={require('./img/logo-1.png')} style= {{width:80, height:90, justifyContent:"center", alignItems:"center", flex:1}} />
+           </CardItem>
+           <CardItem bordered> 
+           <Body>
+              <Item floatingLabel>
+      
+             <Input placeholder='Username' />
+              </Item>
+              <Item floatingLabel last>
+             <Input placeholder='Password' />
+              </Item>
+            </Body>
+           </CardItem>
+           <CardItem  >
+             <Button block success style= {{ height:70, justifyContent:"center", alignItems:"stretch", flex:1}} >
+               <Text style= {{textAlign:"center", marginTop: 10, fontSize:30}}> ENTRAR </Text>
+               </Button>
+           </CardItem>
+            </Card>
+
         </Content>
+
       </Container>
     );
   }
