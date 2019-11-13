ac46865335715819c337e8844b189540068a8a48
gabhu-dev <gabhu@hotmail.es>
2019-11-13 08:44:40 -0500

    reporte maquetacion

---
+++ devfest/Ransa/Components/TabTree.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, List, ListItem, Thumbnail, Text, Left, Body, Right, Button } from 'native-base';
+export default class ListThumbnailExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'Image URL' }} />
+              </Left>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note numberOfLines={1}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'Image URL' }} />
+              </Left>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note numberOfLines={1}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+                 <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'Image URL' }} />
+              </Left>
+              <Body>
+                <Text>Sankhadeep</Text>
+                <Text note numberOfLines={1}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
+  }
+}
--- devfest/Ransa/Screem/Reporte.js
+++ devfest/Ransa/Screem/Reporte.js
@@ -1,24 +1,83 @@
 
 import React, { Component } from "react";
 import { Container, Footer, Content} from 'native-base';
-import { View, Text} from "react-native";
-import AutocompleteInput from '../Components/Autocomplete-input';
+import {StyleSheet, View, Text} from "react-native";
+import Slider from '../Components/Slider';
+import ProgressCircle from 'react-native-progress-circle'
 export default class Reporte extends Component {
     static navigationOptions = {
         title: 'Welcome to Report',
       };
   render() {
+ 
     return (
        <Container>
          <Content>
-        <AutocompleteInput/>
+       
         <View>
-          <Text>TOTAL DE ORDENES</Text>
-           <Text>numero</Text>
-         </View>
+          <View style={styles.viewGeneral}>
+          <ProgressCircle
+            percent={30}
+            radius={50}
+            borderWidth={8}
+            color="#3399FF"
+            shadowColor="#999"
+            bgColor="#fff"
+          >
+            <Text style={{ fontSize: 18 }}>{'30%'}</Text>
+          </ProgressCircle>
+          <View style={styles.viewState}>
+            <View>
+            <Text>5</Text>
+            <Text>PENDIENTE</Text>
+            </View>
+            <View>
+            <Text>5</Text>
+            <Text>PENDIENTE</Text>
+            </View>
+            <View>
+            <Text>5</Text>
+            <Text>PENDIENTE</Text>
+            </View>
+            <View>
+            <Text>5</Text>
+            <Text>PENDIENTE</Text>
+            </View>
+            <View>
+            <Text>5</Text>
+            <Text>PENDIENTE</Text>
+            </View>
+            <View>
+            <Text>5</Text>
+            <Text>PENDIENTE</Text>
+            </View>
+          </View>
+          </View>
+          <Slider/>
+          </View>
+      
         <Footer/> 
         </Content>
       </Container>
     );
   }
 }
+const styles =StyleSheet.create ( {
+  viewGeneral :{
+    backgroundColor:'#009B3A',
+    display:"flex",
+    flexDirection:"row",
+    alignItems:"center",
+    padding:10
+  },
+  viewState:{
+    backgroundColor:'white',
+    display:"flex",
+    flexDirection:"row",
+    flexWrap:"wrap",
+    margin:3, 
+    borderColor:"black",
+    borderWidth: 0.5,
+  }
+  
+})
